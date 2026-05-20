b791e761c32154066a626e07e8573bfc978ab1f8
ErisSelimi <erisselimi3@gmail.com>
2026-05-20 19:00:40 +0200

    new

---
+++ modul29/main.js
@@ -0,0 +1,39 @@
+var programmmingL = ['Java','html','Python'];
+
+console.log(programmmingL);
+
+console.log(programmmingL[1]);
+
+programmmingL.pop();
+console.log(programmmingL);
+
+programmmingL.push('css');
+console.log(programmmingL);
+
+
+programmmingL.unshift('react');
+console.log(programmmingL);
+
+programmmingL.shift();
+console.log(programmmingL);
+
+programmmingL.splice(0,2,'javascript');
+console.log(programmmingL);
+
+
+console.log(Math.random()*100);
+
+console.log(Math.floor(Math.random()*100))
+
+
+var students = ["Darisin","Donartin"];
+
+var [s1,s2] = students;
+
+console.log(s1);
+
+var places = ["prishtine","prizrenin","peja","gjilanin"];
+
+var [, firstplace,,secondplace] = places;
+
+console.log(secondplace);
