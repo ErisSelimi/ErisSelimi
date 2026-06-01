e1b93fb5614315d185f4e0daebe81e36fde725e5
Eris Selimi <erisselimi3@gmail.com>
2026-06-01 19:23:36 +0200

    new

---
+++ modul30/main.js
@@ -0,0 +1,27 @@
+var a = 8;
+do {
+    a = a + 1;
+    console.log(a);
+} while (a > 15);
+
+console.log("----------");
+
+var i = 3;
+while (i < 13) {
+    i = i + 1;
+    console.log(i);
+}
+
+console.log("----------");
+var x = 0;
+var y = 0;
+
+while (y < 4) {
+    y++;
+    x += y;
+    console.log(x);
+    console.log(y);
+}
+for (let b = 0; b < 5; b++) {
+    console.log("vlera e b eshte :" + b);
+}
