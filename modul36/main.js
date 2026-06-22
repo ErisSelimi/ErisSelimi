ee6def2f625432efb94ec7ff657bee105c6611ce
ErisSelimi <erisselimi3@gmail.com>
2026-06-22 19:08:48 +0200

    new

---
+++ modul36/main.js
@@ -0,0 +1,7 @@
+$(window).ready(function(){
+    alert('It is working');
+    $('#test').css('color','red');
+    $('.test2').css('background-color','darkblue');
+    $('#myform :input').css('border','3px solid red');
+    $('div:visible').css('color', 'green');
+});
